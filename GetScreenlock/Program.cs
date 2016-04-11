18a80bb3b2a9370501680e164bfa02ae8a3a3717
unknown <>
2016-04-11 21:28:15 +0800

    first commit

---
+++ GetScreenlock/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GetScreenlock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            string screenlockFa = null;
+            Console.WriteLine("###Author Co1In");
+            Console.WriteLine(@"###File Path: C:\Background");
+            DirectoryInfo screenlockRoot = new DirectoryInfo(@"C:\Users\Co1In\AppData\Local\Packages");
+            foreach (DirectoryInfo item in screenlockRoot.GetDirectories())
+            {
+                if (item.Name.Contains("Microsoft.Windows.ContentDeliveryManager"))
+                {
+                    screenlockFa = item.FullName;
+                }
+            }
+
+            DirectoryInfo screenlockDI = new DirectoryInfo(screenlockFa + @"\LocalState\Assets");
+            foreach (FileInfo item in screenlockDI.GetFiles())
+            {
+                if (item.Length > 102400)
+                {
+                    if (!Directory.Exists(@"C:\Background"))
+                    {
+                        Directory.CreateDirectory(@"C:\Background");
+                    }
+                    try {
+                        item.CopyTo(@"C:\Background\" + DateTime.Now.ToString("yyyy-MM-dd") + "-" + i + ".jpg");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    Console.WriteLine("Copy {0} -> C:\\Background\\", item.Name);
+                    i++;
+                }
+            }
+            Console.WriteLine("#Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
